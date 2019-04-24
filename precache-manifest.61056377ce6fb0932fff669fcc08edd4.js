6023392f51109883b382dee8662cae7ea1e56358
HaydenRoeder1 <hroeder1@binghamton.edu>
2019-04-24 12:03:54 -0400

    Initial Commit

---
+++ precache-manifest.61056377ce6fb0932fff669fcc08edd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3b9d5636476b036b2ef",
+    "url": "/static/css/main.5ab3e586.chunk.css"
+  },
+  {
+    "revision": "f3b9d5636476b036b2ef",
+    "url": "/static/js/main.37a9282d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "21d2d96d8e4cb321570a",
+    "url": "/static/css/2.bf262607.chunk.css"
+  },
+  {
+    "revision": "21d2d96d8e4cb321570a",
+    "url": "/static/js/2.c8622d7e.chunk.js"
+  },
+  {
+    "revision": "7d101b37c03ae8988b840bf2d916ed1f",
+    "url": "/index.html"
+  }
+];
